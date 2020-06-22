3013cec4765b7e77b8da1dc1a44d9aa63d666b6c
pablo <pablobonilla11@hotmail.com>
2020-06-21 21:28:38 -0400

    21-6-2020 Final

--- js/mis-scripts.js
+++ js/mis-scripts.js
@@ -1,35 +1,34 @@
-$(function(){
+$(function() {
 
- /* =============================================
+    /* =============================================
 1. Configuración de Rd-navbar
 ============================================= */
-$('.rd-navbar').RDNavbar({
-    stickUpClone:false,
-    responsive:{
-        0:{
-            layout: 'rd-navbar-fixed'
-        },
-        768:{
-            layout: 'rd-navbar-static',
-            stickUp:false
-        },
-        992:{
-            stickUp:false
+    $('.rd-navbar').RDNavbar({
+        stickUpClone: false,
+        responsive: {
+            0: {
+                layout: 'rd-navbar-fixed'
+            },
+            768: {
+                layout: 'rd-navbar-static',
+                stickUp: false
+            },
+            992: {
+                stickUp: false
+            }
+
+
+
         }
+    });
 
-
-
-
-    }
-});
-
- /* =============================================
+    /* =============================================
 2. Configuración de Universal Parallax
 ============================================= */
 
-new universalParallax().init({
-	speed: 6.0
-});
+    new universalParallax().init({
+        speed: 6.0
+    });
 
 
 });
